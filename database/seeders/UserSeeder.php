d1cd0bbd4c4f2820c09de1807d8642ac44267002
Saidshoh Barotov <saidshoh.barotov@tcell.tj>
2022-06-15 12:08:50 +0500

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,6 +18,7 @@ class UserSeeder extends Seeder
             ['name' => 'Саидшох Баротов',  'avatar' => 'images/default.png', 'phone' => '902222900', 'ball' => 5, 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'Ахмадчони М.',  'avatar' => 'images/01.png', 'phone' => '934737318', 'ball' => 556, 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'Шахриёр Гафуров',  'avatar' => 'images/022.png',  'phone' => '933713121', 'ball' => 557, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Zafar Davronovich',  'avatar' => 'images/default.png',  'phone' => '930000000', 'ball' => 450, 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
