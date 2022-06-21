b76a8d9e58f6dbbedc024de3f2ae38fe076a4876
Saidshoh Barotov <saidshoh.barotov@tcell.tj>
2022-06-21 14:39:16 +0500

    Update NotificationService.php

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -7,12 +7,12 @@ class NotificationService
 {
     public function sendSms($phone, $code)
     {
-        $basic  = new \Vonage\Client\Credentials\Basic("0cf3b534", "5Gdv7qWLUOuktEdj");
-        $client = new \Vonage\Client($basic);
+        // $basic  = new \Vonage\Client\Credentials\Basic("0cf3b534", "5Gdv7qWLUOuktEdj");
+        // $client = new \Vonage\Client($basic);
 
-        $response = $client->sms()->send(
-            new \Vonage\SMS\Message\SMS("992".$phone, 'CheckIn', 'Код для подтверждения: '.$code)
-        );
+        // $response = $client->sms()->send(
+        //     new \Vonage\SMS\Message\SMS("992".$phone, 'CheckIn', 'Код для подтверждения: '.$code)
+        // );
 
         $message = $response->current();
 
