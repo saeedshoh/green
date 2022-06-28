7b5104a93d8d69af05996d77c2855762c2fe07e3
Saidshoh Barotov <saidshoh.barotov@tcell.tj>
2022-06-28 17:36:44 +0500

    Update app.blade.php

--- resources/views/dashboard/app.blade.php
+++ resources/views/dashboard/app.blade.php
@@ -5,8 +5,9 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/x-icon" href="/images/default.png">
 
-    <title>Dashboard - {{ $title }}</title>
+    <title>U-Project | {{ $title }}</title>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
